76782bd4d68042114c92b2f714c7dc16b9e82ae7
Tejas Mehta <tmthecoder@gmail.com>
2020-10-05 12:28:30 -0400

    try adding a method
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/functions-src/apple_sign_in_crossclip.js
+++ web/netlify-src/functions-src/apple_sign_in_crossclip.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-require("./shared/apple_sign_in.js");
+var signIn = require("./shared/apple_sign_in.js");
 
 exports.handler = async function(event) {
-    signInWithApple(event, process.env.CROSSCLIP_ANDROID_BUNDLE_ID)
+    signIn.signInWithApple(event, process.env.CROSSCLIP_ANDROID_BUNDLE_ID)
 }
--- web/netlify-src/functions-src/shared/apple_sign_in.js
+++ web/netlify-src/functions-src/shared/apple_sign_in.js
@@ -1,17 +1,20 @@
-function signInWithApple(event, packageID) {
-    if (event.httpMethod !== "POST") {
-        return {
-            statusCode,
-            headers,
-            body: "This was not a POST request!"
-        };
+
+module.exports = {
+    signInWithApple: function(event, packageID){
+        if (event.httpMethod !== "POST") {
+            return {
+                statusCode,
+                headers,
+                body: "This was not a POST request!"
+            };
+        }
+        const redirect = `intent://callback?${new URLSearchParams(
+            request.body
+        ).toString()}#Intent;package=${
+            packageID
+        };scheme=signinwithapple;end`;
+        console.log(`Redirecting to ${redirect}`);
+        response.redirect(307, redirect);
     }
-    const redirect = `intent://callback?${new URLSearchParams(
-        request.body
-    ).toString()}#Intent;package=${
-        packageID
-    };scheme=signinwithapple;end`;
-    console.log(`Redirecting to ${redirect}`);
-    response.redirect(307, redirect);
 }
 
