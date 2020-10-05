c5d11a0e4287a3e77463f40b654397bedf7ca442
Tejas Mehta <tmthecoder@gmail.com>
2020-10-05 12:13:45 -0400

    fix errors
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/functions-src/apple_sign_in_crossclip.js
+++ web/netlify-src/functions-src/apple_sign_in_crossclip.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-require("./shared/apple_sign_in");
+require("./shared/apple_sign_in.js");
 
 exports.handler = async function(event) {
     signInWithApple(event, process.env.CROSSCLIP_ANDROID_BUNDLE_ID)
