de50d409f5e90123427f273dcb5054f0b26a4170
Tejas Mehta <tmthecoder@gmail.com>
2020-10-05 14:08:21 -0400

    test redirects one more damn timee
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/functions-src/apple_sign_in_crossclip.js
+++ web/netlify-src/functions-src/apple_sign_in_crossclip.js
@@ -3,32 +3,10 @@ var signIn = require("./shared/apple_sign_in.js");
 
 exports.handler = function(event, context, callback) {
     var location = signIn.signInWithApple(event, process.env.CROSSCLIP_ANDROID_BUNDLE_ID)
-    // callback(null, {
-    //     statusCode: 302,
-    //     headers: {
-    //         Location: location,
-    //         'Cache-Control': 'no-cache' // Disable caching of this response
-    //     },
-    //     body: '' // return body for local dev
-    // })
-    console.log(location)
-    if (event.httpMethod !== "POST") {
-        return {
-            statusCode,
-            headers,
-            body: "This was not a POST request!"
-        };
-    }
-    const redirect = `intent://callback?${new URLSearchParams(
-        event.body
-    ).toString()}#Intent;package=${
-        packageID
-    };scheme=signinwithapple;end`;
-    console.log(`Redirecting to ${redirect}`);
     callback(null, {
         statusCode: 302,
         headers: {
-            Location: redirect,
+            Location: location,
             'Cache-Control': 'no-cache' // Disable caching of this response
         },
         body: '' // return body for local dev
