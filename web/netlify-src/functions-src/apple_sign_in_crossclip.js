bdaa7cca6c4189c4b4dcd03c9705c6cfe5f06b18
Tejas Mehta <tmthecoder@gmail.com>
2020-10-05 12:08:06 -0400

    add a sign in with apple callback
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/functions-src/apple_sign_in_crossclip.js
+++ web/netlify-src/functions-src/apple_sign_in_crossclip.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-require("./apple_sign_in");
+require("./shared/apple_sign_in");
 
 exports.handler = async function(event) {
     signInWithApple(event, process.env.CROSSCLIP_ANDROID_BUNDLE_ID)
