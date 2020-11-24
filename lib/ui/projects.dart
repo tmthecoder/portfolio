28ad174f7a0278d384ec6fe59f882b7c312d1be3
Tejas Mehta <tmthecoder@gmail.com>
2020-11-24 10:40:17 -0500

    Add OdometryCore to the Developers section
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- lib/ui/projects.dart
+++ lib/ui/projects.dart
@@ -54,7 +54,10 @@ class ProjectsState extends State<Projects> with WidgetsBindingObserver {
   ///Builds the UI on this screen
   @override
   Widget build(BuildContext context) {
-    // Get the width and use it to get the axis count and dynamic padding size
+    //Get both item lists
+    List<Widget> userItems = getUserItems();
+    List<Widget> developerItems = getDeveloperItems();
+    //Get the width and use it to get the axis count and dynamic padding size
     double width = MediaQuery.of(context).size.width;
     double paddingSize = CommonMethods.getDynamicPaddingSize(width, width/5000/1.5);
     int axisCount = CommonMethods.getGridAxisCount(MediaQuery.of(context).size.width, paddingSize, 300);
@@ -74,8 +77,8 @@ class ProjectsState extends State<Projects> with WidgetsBindingObserver {
               crossAxisCount: axisCount,
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
-              children: getUserItems(),
-              staggeredTiles: List.filled(4, StaggeredTile.fit(1)),
+              children: userItems,
+              staggeredTiles: List.filled(userItems.length, StaggeredTile.fit(1)),
             ),
           ),
           SliverPadding(
@@ -91,8 +94,8 @@ class ProjectsState extends State<Projects> with WidgetsBindingObserver {
               crossAxisCount: axisCount,
               crossAxisSpacing: 10,
               mainAxisSpacing: 10,
-              children: getDeveloperItems(),
-              staggeredTiles: List.filled(1, StaggeredTile.fit(1)),
+              children: developerItems,
+              staggeredTiles: List.filled(developerItems.length, StaggeredTile.fit(1)),
             ),
           )
         ],
@@ -104,12 +107,15 @@ class ProjectsState extends State<Projects> with WidgetsBindingObserver {
   /// [UserProjectListItem] as each item will signify a different user-oriented project of mine
   List<Widget> getUserItems() {
     return [
+      //CrossClip's listing
       UserProjectListItem(
         image: AssetImage("assets/projects/crossclip/website_logo_${ThemeController.of(context).isDark ? "dark" : "light"}.png"),
         title: "CrossClip",
         description: "A secure, versatile, and seamless cross-platform clipboard manager and file transfer agent",
         projectStatus: ProjectStatus.DEVELOPMENT,
       ),
+
+      //FTC Scouting and Scoring's listing
       UserProjectListItem(
         image: AssetImage("assets/projects/ftcscoutscore/website_logo.png"),
         title: "FTC Scouring and Scoring",
@@ -117,12 +123,15 @@ class ProjectsState extends State<Projects> with WidgetsBindingObserver {
         projectStatus: ProjectStatus.RELEASED,
       ),
 
+      //blinkOS's listing
       UserProjectListItem(
         image: AssetImage("assets/projects/blinkos/website_logo_${ThemeController.of(context).isDark ? "dark" : "light"}.png"),
         title: "blinkOS - A hackSugar Project",
         description: "An Android-based operating system build to ensure the user's privacy, security, and anonymity.",
         projectStatus: ProjectStatus.ALPHA,
       ),
+
+      //Weasel's listing
       UserProjectListItem(
         image: AssetImage("assets/projects/weasel/website_logo.png"),
         title: "Weasel - A hackSugar Project",
@@ -136,6 +145,7 @@ class ProjectsState extends State<Projects> with WidgetsBindingObserver {
   /// [DeveloperProjectListItem] as each item will signify a different developer-oriented project of mine
   List<Widget> getDeveloperItems() {
     return [
+      //dargon2's listing
       DeveloperProjectListItem(
         title: "dargon2",
         description: "A simple, versatile, and full-featured library for hashing in dart with the Argon2 Password Hash Algorithm",
@@ -152,6 +162,23 @@ class ProjectsState extends State<Projects> with WidgetsBindingObserver {
               link: "https://pub.dev/packages/dargon2",
               assetUri: "assets/third_party/dart_logo.png",
               edgeInsets: const EdgeInsets.fromLTRB(10.0, 10.0, 8.0, 10.0),
+            ),
+          ],
+        ),
+      ),
+
+      //OdometryCore's Listing
+      DeveloperProjectListItem(
+        title: "Odometry Core",
+        description: "A library that houses and abstracts the core algorithm for three-encoder robot motion tracking",
+        projectStatus: ProjectStatus.BETA,
+        linkRow: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ImageLink(
+              link: "https://github.com/tmthecoder/OdometryCore",
+              assetUri: "assets/third_party/github_logo_${ThemeController.of(context).isDark ? "dark" : "light"}.png",
+              edgeInsets: const EdgeInsets.all(10),
             ),
           ],
         ),
