08ba83cfe019cd65f5be48980bb4d11ab711d46f
Tejas Mehta <tmthecoder@gmail.com>
2021-03-19 13:58:28 -0400

    change the direct transfer handler
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/functions-src/direct_transfer_notification_handler.js
+++ web/netlify-src/functions-src/direct_transfer_notification_handler.js
@@ -1,24 +1,46 @@
-var admin = require("firebase-admin")
+var google = require('googleapis');
+var https = require('https');
 
 exports.handler = async function(event, context) {
-    admin.initializeApp({
-        credential: admin.credential.cert({
-            projectId: process.env.CROSSCLIP_FIREBASE_ID,
-            clientEmail: process.env.CROSSCLIP_FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.CROSSCLIP_PRIVATE_KEY.replace(/\\n/g, '\n')
-        }),
-        databaseURL: "https://crossclip-4c8bb.firebaseio.com/"
-    });
-    try {
-        await admin.database().ref("test").set("HI")
-        return {
-            statusCode: 200,
-            body: 'Success!'
-        }
-    } catch (err) {
-        return {
-            statusCode: 500,
-            body: err.toString(),
+    const token = await getAccessToken();
+    const options = {
+        hostname: 'fcm.googleapis.com',
+        path: '/v1/projects/' + process.env.CROSSCLIP_FIREBASE_ID + '/messages:send',
+        method: 'POST',
+        headers: {
+            'Authorization': token
+        },
+        body: {
+            notification: {
+                title: "Direct Transfer Request",
+                body: "Test Request"
+            }
         }
     }
+    const reqeuest = https.request(options, function(resp) {
+        console.log(resp)
+    })
 }
+
+function getAccessToken() {
+    return new Promise(function(resolve, reject) {
+        var SCOPES = [
+            'https://www.googleapis.com/auth/firebase.messaging'
+        ];
+        const jwtClient = new google.auth.JWT(
+            process.env.CROSSCLIP_FIREBASE_CLIENT_EMAIL,
+            null,
+            process.env.CROSSCLIP_PRIVATE_KEY,
+            SCOPES,
+            null
+        );
+        jwtClient.authorize(function(err, tokens) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(tokens.access_token);
+        });
+    });
+}
+
