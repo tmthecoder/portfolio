d95198f5c69b0353bd40488fde6c3e751e51937e
Tejas Mehta <tmthecoder@gmail.com>
2021-03-19 19:01:04 -0400

    remove async
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/functions-src/direct_transfer_notification_handler.js
+++ web/netlify-src/functions-src/direct_transfer_notification_handler.js
@@ -1,38 +1,37 @@
 var {google} = require('googleapis');
 var https = require('https');
 
-exports.handler = async function(event, context) {
-    const token = await getAccessToken();
-    const options = {
-        hostname: 'fcm.googleapis.com',
-        path: '/v1/projects/' + process.env.CROSSCLIP_FIREBASE_ID + '/messages:send',
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + token
-        },
-        body: {
-            "message" : {
-                "name": "direct-transfer",
-                "topic": "allDevices",
-                "notification": {
-                    "title": "Direct Transfer Request",
-                    "body": "Test Request"
-                }
+exports.handler = function(event, context) {
+    getAccessToken().then((token) => {
+        const options = {
+            hostname: 'fcm.googleapis.com',
+            path: '/v1/projects/' + process.env.CROSSCLIP_FIREBASE_ID + '/messages:send',
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + token
             },
+            body: {
+                "message" : {
+                    "name": "direct-transfer",
+                    "topic": "allDevices",
+                    "notification": {
+                        "title": "Direct Transfer Request",
+                        "body": "Test Request"
+                    }
+                },
+            }
         }
-    }
-    console.log("sending")
-    console.log(token)
-    var request = https.request(options, function (res) {
-        console.log("statusCode: ", res.statusCode);
-        console.log("headers: ", res.headers);
+        console.log("sending")
+        console.log(token)
+        var request = https.request(options, function (res) {
+            console.log("statusCode: ", res.statusCode);
+            console.log("headers: ", res.headers);
+        });
+        request.on('error', function(e) {
+            console.error('error');
+            console.error(e);
+        });
     });
-
-    request.on('error', function(e) {
-        console.error('error');
-        console.error(e);
-    });
-
 }
 
 function getAccessToken() {
