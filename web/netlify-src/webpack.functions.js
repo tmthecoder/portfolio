a90b368cd8d8278afd66548a4d439cfc0186a7d5
Tejas Mehta <tmthecoder@gmail.com>
2021-03-19 13:27:08 -0400

    another trial
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/webpack.functions.js
+++ web/netlify-src/webpack.functions.js
@@ -1,8 +1,34 @@
-const nodeExternals = require('webpack-node-externals');
+const GenerateJsonPlugin = require('generate-json-webpack-plugin')
+const externals = [
+    'firebase-admin',
+    'lodash'
+]
+
+const genPackage = () => ({
+    name: 'functions',
+    private: true,
+    main: 'direct_transfer_notification_handler.js',
+    author:"@tmthecoder",
+    license: 'MIT',
+    dependencies: externals.reduce(
+        (acc, name) =>
+            Object.assign({}, acc, {
+                [name]:
+                pkg.dependencies[name] ||
+                pkg.devDependencies[name]
+            }),
+        {}
+    )
+})
 
 module.exports = {
-    externals: [nodeExternals()],
+    target: 'node',
     resolve: {
         mainFields: ['module', 'main']
     },
+    externals: externals.reduce(
+        (acc, name) => Object.assign({}, acc, { [name]: true }),
+        {}
+    ),
+    plugins: [new GenerateJsonPlugin('package.json', genPackage())]
 }
