81e09dbff3ccfb308995aac6a51f9a2266898b7a
Tejas Mehta <tmthecoder@gmail.com>
2021-03-19 12:50:49 -0400

    update process variables
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/netlify-src/functions-src/direct_transfer_notification_handler.js
+++ web/netlify-src/functions-src/direct_transfer_notification_handler.js
@@ -1,18 +1,11 @@
 var admin = require("firebase-admin")
-var serviceAccount = process.env.FIREBASE_ADMIN_CREDENTIALS
 
 exports.handler = async function(event, context) {
-    const projId = JSON.parse(serviceAccount).project_id;
-    console.log("ID: " + projId)
-    const clientEmail = JSON.parse(serviceAccount).client_email;
-    console.log("EMAIL: " + clientEmail)
-    const privKey = JSON.parse(serviceAccount).private_key;
-    console.log("PRIVKEY: " + privKey)
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: JSON.parse(serviceAccount).project_id,
-            clientEmail: JSON.parse(serviceAccount).client_email,
-            privateKey: JSON.parse(serviceAccount).private_key.replace(/\\n/g, '\n')
+            projectId: process.env.CROSSCLIP_FIREBASE_ID,
+            clientEmail: process.env.CROSSCLIP_FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.CROSSCLIP_PRIVATE_KEY.replace(/\\n/g, '\n')
         }),
         databaseURL: "https://crossclip-4c8bb.firebaseio.com/"
     });
