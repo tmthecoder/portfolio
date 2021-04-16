7e9d4f0cd1d236bfd64937ce25d236f177a65269
Tejas Mehta <tmthecoder@gmail.com>
2021-04-16 13:31:46 -0400

    move path based on projects for cloudflare api
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

--- web/api/src/handler.ts
+++ web/api/src/handler.ts
@@ -6,9 +6,9 @@ import DirectTransferNotification from "./cross_clip/direct_transfer_notificatio
 const router = Router()
 
 router
-    .post('/apple_sign_in', AppleSignInCrossClip)
+    .post('/cross_clip/apple_sign_in', AppleSignInCrossClip)
     .post('/scouting_scoring/apple_sign_in', AppleSignInScoutScore)
-    .post('/direct_transfer_notification', DirectTransferNotification)
+    .post('/cross_clip/direct_transfer_notification', DirectTransferNotification)
     .all('*', () => new Response("Not found", { status: 404 }))
 
 export const handleRequest = (request: Request) => {
